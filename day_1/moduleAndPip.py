f0ba9f8db3657154788e1ad15254e601ea466c41
iaruna <arunakumariofficial@gmail.com>
2023-03-08 22:47:08 +0530

    initial commit

---
+++ day_1/moduleAndPip.py
@@ -0,0 +1,8 @@
+# Module types are two
+# Built-in Modules like: abs(), delattr(), hash(), set(), dict() etc.
+# External Module like: pandas, numpy
+
+import pandas as pd
+
+df = pd.read_csv("submission.csv")
+print(df)
