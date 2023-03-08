9a7b4a6e9641dac1f5641afd1c354a03c86e131f
iaruna <arunakumariofficial@gmail.com>
2023-03-08 23:18:54 +0530

    comments Escape sequence

---
+++ day_1/commentSequence.py
@@ -0,0 +1,19 @@
+# This is a 'Single-line comment'
+print("This is a print statement.")
+print('Hello Life!!!!')
+print("I am a programmar who is now on 'Beginner' state.")
+
+''' This is an if-else statement.  It will execute a block of
+ code if a specified condition is true or false.(This is a 
+ multi- line comment)'''
+A = 5
+if(A>4):
+    print("A is greater than 4.")
+else:
+    print("A is not greater than 4.")
+
+# backslash (\) use
+# print("This doesnot "execute") # Through error
+print("This will\"execute")
+
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
