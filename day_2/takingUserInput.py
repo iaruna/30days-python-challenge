b1da41c5919d9d96e81a5ecf3c7a301553fed99f
iaruna <arunakumariofficial@gmail.com>
2023-03-09 23:20:52 +0530

    Taking input by user

---
+++ day_2/takingUserInput.py
@@ -0,0 +1,9 @@
+# variable = input() 
+# input function returns value as string.
+
+variable1 = int(input("Enter first number: "))
+variable2 = float(input("Enter second number: "))
+print(variable1 + variable2)
+
+Name = input("Enter your name: ")
+print("My name is", Name)
