f329c1ae7d4de2fa9924eda240b4bdd83c1064ec
iaruna <arunakumariofficial@gmail.com>
2023-03-09 17:53:46 +0530

    variables and datatypes

---
+++ day_2/variablesAndDatatypes.py
@@ -0,0 +1,40 @@
+# These are variables of different data types.
+a = 7.53
+print(a)
+print(type(a)) # float
+
+b = True
+print(b)
+print(type(b)) # bool
+
+c = "Aruna"
+print(c)
+print(type(c)) # String
+
+d = None
+print(d)
+print(type(d)) # None
+
+e = "1"
+print(e)
+print(type(e)) # String
+
+f = 1
+print(f)
+print(type(f)) # int
+
+g = complex(7, 2)
+print(g)
+print(type(g)) # Complex
+
+list = [2, 4.3, -9, 7, 2, ["Apple", "Banana"] ]
+print(list)
+print(type(list)) # List
+
+tuple = (("Banana", "Lichi"),("Cat", "Dog"))
+print(tuple)
+print(type(tuple)) # Tuple
+
+dict = {"Name": "Aruna", "Age": 23, "CanVote": True}
+print(dict)
+print(type(dict)) # Dict
