d73f84d566fef8be04902926215158dbc1944172
iaruna <arunakumariofficial@gmail.com>
2023-03-09 18:50:58 +0530

    Type casting

---
+++ day_2/typeCasting.py
@@ -0,0 +1,21 @@
+# Implicit typecasting:
+a = 51
+print(type(a))
+
+b = 13.0
+print(type(b))
+
+c = a + b
+print(c)
+print(type(c))
+
+d = "71"
+e = "94"
+print(int(d) + int(e))
+
+# Emplicit typecasting:
+string = "115"
+number = 51
+string1 = int(string)
+print("The sum of two numbers is: ", string1 + number)
+
