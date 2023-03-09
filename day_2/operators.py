45c8dc68e57578c986f6b1d56d26728aee3bd43b
iaruna <arunakumariofficial@gmail.com>
2023-03-09 23:10:37 +0530

    .\operators.py

---
+++ day_2/operators.py
@@ -0,0 +1,21 @@
+# Arithmetic Operators
+x = 15
+y = 7
+
+print(f"Addition:", x + y)
+print(f"Subtraction:", x - y)
+print(f"Multiplication:", x * y)
+print(f"Division:", x / y)	
+print(f"Modulus:", x % y)
+print(f"Exponentiation:", x ** y)
+print(f"Floordivision:", x // y)
+
+# Assignment Operators
+print(x) 
+x += 3
+print(x)
+x -= 3 # same as x = x - 3
+print(x)
+
+# Comparison Operators
+print(x == y)
