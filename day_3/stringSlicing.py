7fce0c12a49e8e27dd92e19185879a27be23f94a
iaruna <arunakumariofficial@gmail.com>
2023-03-10 19:12:06 +0530

    String Slicing

---
+++ day_3/stringSlicing.py
@@ -0,0 +1,16 @@
+fruit ="Mango"
+length = len(fruit)
+print("Mango is a" ,length, "letter word" )
+
+pie = "ApplePie"
+print(pie[0:])
+print(pie[:4])
+print(pie[2:7])
+print(pie[-5:-1])
+
+alphabets = "ABCDEFG"
+for i in alphabets:
+    print(i)
+
+name = "Aruna"
+print(name[-4:-2])
