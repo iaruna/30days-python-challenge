e62ff4439ac371858a054e4c0039fbe10580a614
iaruna <arunakumariofficial@gmail.com>
2023-03-10 17:54:50 +0530

    Strings

---
+++ day_3/strings.py
@@ -0,0 +1,25 @@
+name = "Aruna"
+print("Hello,",name)
+friend = "Sayali"
+print("Hello, " + friend)
+
+anotherName = "Aryan"
+print(anotherName)
+print(friend[0])
+print(friend[1])
+print(friend[2])
+print(friend[3])
+print(friend[4])
+print(friend[5])
+
+print("He said,'Call me at 7PM'.")
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+print("\nlets use a for loop")
+for character in anotherName:
+    print(character)
