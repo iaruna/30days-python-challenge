5b8148cb8c0ac7e36c4105d053eca25e73298e2c
iaruna <arunakumariofficial@gmail.com>
2023-03-11 23:09:02 +0530

    For Loop

---
+++ day_4/forLoop.py
@@ -0,0 +1,15 @@
+name = "Aruna"
+for i in name:
+    # print(i)
+    print(i, end =",")
+    if (i == "a"):
+        print("this is something special!\t")
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+print("\t")
+for k in range(1, 100, 2):
+    print(k)
