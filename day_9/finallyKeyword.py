decfcd90d6a59616fecdc37578b6213ffdd9ae76
iaruna <arunakumariofficial@gmail.com>
2023-03-16 23:47:02 +0530

    Finally Keyword

---
+++ day_9/finallyKeyword.py
@@ -0,0 +1,15 @@
+def func1():
+    try:
+        l = [2, 5, 7, 3]
+        i = int(input("Enter the index: "))
+        print(l[i])
+        return 1
+    except:
+        print("Some error occurred")
+        return 0
+
+    finally:
+        print("I am always executed")
+    print("I am always executed")
+x = func1()
+print(x)
