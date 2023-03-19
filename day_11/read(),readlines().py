d0e7db0fbed01298d2f062d6a03e6c5a39afcf83
iaruna <arunakumariofficial@gmail.com>
2023-03-20 00:10:11 +0530

    read and readlines

---
+++ day_11/read(),readlines().py
@@ -0,0 +1,31 @@
+f = open("file.txt", "r")
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(line)
+
+f = open("myfile", "r")
+i = 0
+while True:
+    i = i + 1
+    line = f.readline()
+    if not line:
+        break
+    m1 = int(line.split(",")[0])
+
+    m2 = int(line.split(",")[1])
+    m3 = int(line.split(",")[2])
+    print(f"Marks of student {i} in Maths is: {m1 * 2}")
+    print(f"Marks of student {i} in Hindi is: {m2 * 2}")
+    print(f"Marks of student {i} in English is: {m3 * 2}")
+    print(line)
+
+# f = open('myfile2.txt', 'w')
+# # lines = ['line 1\n', 'line 2\n', 'line 3\n']
+# # f.writelines(lines)
+# # f.close()
+# lines = ["line1", "line2", "line3"]
+# for line in lines:
+#     f.write(line + "\n")
+# f.close()
