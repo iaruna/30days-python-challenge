b6c3c49dc05f978f9cc4a6cbfb11dc27e361abc5
iaruna <arunakumariofficial@gmail.com>
2023-03-21 21:09:27 +0530

    Is Vs ==

---
+++ day_13/isVs==.py
@@ -0,0 +1,12 @@
+a = 4
+b = "4"
+c = [1, 23, 54]
+d = [1, 23, 54]
+e = 3
+f = 3
+print(a is b)  # exact location of object in memory/ compare identity
+print(a == b)  # value
+print(c == d)
+print(c is d)
+print(e == f)
+print(e is f)
