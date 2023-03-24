2ca0d745e1b9719d0c8818dc5147404d45d65c99
iaruna <arunakumariofficial@gmail.com>
2023-03-24 23:09:32 +0530

    dir dict and help

---
+++ day_16/dirDict.py
@@ -0,0 +1,15 @@
+x = [1, 2, 4]
+# print(dir(x))
+print(x.__add__)
+
+
+# class person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self.version = 1
+
+
+# p = person("John", 30)
+# print(p.__dict__)
+# print(help(person))
