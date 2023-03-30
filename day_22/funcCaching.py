8b074a87b8e4241ce6c6d47e599f44c8e72a1d5b
iaruna <arunakumariofficial@gmail.com>
2023-03-30 15:33:10 +0530

    function caching

---
+++ day_22/funcCaching.py
@@ -0,0 +1,34 @@
+from functools import lru_cache
+import time
+
+
+@lru_cache(maxsize=None)
+def func(x):
+    time.sleep(4)
+    return x*9
+
+
+print(func(9))
+print("Done for 9")
+print(func(81))
+print("Done for 81")
+print(func(79))
+print("Done for 79")
+print(func(69))
+print("Done for 69")
+print(func(59))
+print("Done for 59")
+print(func(49))
+print("Done for 49")
+print(func(39))
+print("Done for 39")
+print(func(49))
+print("Done for 49")
+print(func(59))
+print("Done for 59")
+print(func(69))
+print("Done for 69")
+print(func(79))
+print("Done for 79")
+print(func(89))
+print("Done for 89")
